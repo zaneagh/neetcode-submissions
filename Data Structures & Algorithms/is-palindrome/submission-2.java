572a040f1eeb057290edc446793220535a192fae
zaneagh <141767974+zaneagh@users.noreply.github.com>
2026-07-27 17:55:31 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+     public boolean isPalindrome(String s) {
+        StringBuilder newStr = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                newStr.append(Character.toLowerCase(c));
+            }
+        }
+        return newStr.toString().equals(newStr.reverse().toString());
+    }
+}
